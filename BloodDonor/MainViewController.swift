ef2e41a55f767d0d1966471ea332315babcbd9d0
Lukasz Cettler <lukasz.cettler@gmail.com>
2023-02-19 21:29:24 +0100

    DEV-13 Add ViewControllers and TabBarItems

---
+++ BloodDonor/MainViewController.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func commonInit() {
+        
+    }
+    
+    func setTabBarImage(imageName: String, title: String, tag: Int) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+        
+        
+    }
+
+
+}
+
