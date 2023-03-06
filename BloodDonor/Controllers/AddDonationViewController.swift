2a17cc525d1e7f2a85195ec4ff1e6bdd8289c778
Lukasz Cettler <lukasz.cettler@gmail.com>
2023-03-06 21:19:04 +0100

    DEV-15 Add FAB button to HomeView

--- BloodDonor/Controllers/AddDonationViewController.swift
+++ BloodDonor/Controllers/AddDonationViewController.swift
@@ -4,15 +4,26 @@ class AddDonationViewController: MainViewController {
     override func viewDidLoad() {
         let colorWhite = UIColor(red: 253, green: 251, blue: 247, alpha: 1)
         view.backgroundColor = colorWhite
-        title = "Add Donation"
-        setupNavBar()
     }
     
-    override func commonInit() {
-        setTabBarImage(imageName: "plus.circle.fill", title: "Add", tag: 3)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let actionView = UIAlertController(title: "Dodaj wpis do dziennika", message: nil, preferredStyle: .actionSheet)
+        actionView.addAction(UIAlertAction(title: "❤️ Dodaj donację", style: .default, handler: openDetails))
+        actionView.addAction(UIAlertAction(title: "💔 Dyskwalifikacja", style: .default, handler: openDetails))
+        actionView.addAction(UIAlertAction(title: "Anuluj", style: .cancel))
+
+        // Set the popover presentation controller to show the action sheet from the tab bar item
+        if let tabBarController = self.tabBarController {
+            actionView.popoverPresentationController?.sourceView = tabBarController.tabBar
+            actionView.popoverPresentationController?.sourceRect = tabBarController.tabBar.bounds
+        }
+
+        present(actionView, animated: true, completion: nil)
     }
     
-    func setupNavBar() {
-        navigationController?.navigationBar.topItem?.title = "Add donation!"
+    func openDetails(action: UIAlertAction) {
+        let formVC = FormViewController()
     }
 }
