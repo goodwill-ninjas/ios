5ca35f1831f8976698fc918a7c9ad2750a3d025c
Adam Jurkiewicz <a.jurkiewicz5@gmail.com>
2023-05-01 08:16:40 +0200

    DEV-94 Change bottom nav order to match Android (#4)

--- BloodDonor/AppDelegate.swift
+++ BloodDonor/AppDelegate.swift
@@ -22,30 +22,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Create instances of the HomeViewController, UserViewController, AddDonationViewController, DonationsViewController, and MapViewController classes
         let homeVC = HomeViewController()
-        let userVC = UserViewController()
-        let addDonationVC = AddDonationViewController()
         let donationsVC = DonationsViewController()
+        let addDonationVC = AddDonationViewController()
         let mapVC = MapViewController()
+        let userVC = UserViewController()
         
         // Create instances of the UINavigationController class with each view controller as its root view controller
         let homeNC = UINavigationController(rootViewController: homeVC)
-        let userNC = UINavigationController(rootViewController: userVC)
-        let addDonationNC = UINavigationController(rootViewController: addDonationVC)
         let donationsNC = UINavigationController(rootViewController: donationsVC)
+        let addDonationNC = UINavigationController(rootViewController: addDonationVC)
         let mapNC = UINavigationController(rootViewController: mapVC)
+        let userNC = UINavigationController(rootViewController: userVC)
         
         // Create an instance of the UITabBarController class
         let tabBarController = UITabBarController()
         
         // Set the view controllers of the tab bar controller to the navigation controllers created earlier
-        tabBarController.viewControllers = [homeNC, userNC, addDonationNC, donationsNC, mapNC]
+        tabBarController.viewControllers = [homeNC, donationsNC, addDonationNC, mapNC, userNC]
         
         // Create instances of UITabBarItem and set their properties
-        let tabBarItemHome = UITabBarItem(title: "Home", image: UIImage(systemName: "house.fill"), tag: 0)
-        let tabBarItemUser = UITabBarItem(title: "User", image: UIImage(systemName: "person.fill"), tag: 1)
-        let tabBarItemAddDonation = UITabBarItem(title: "Add donation", image: UIImage(systemName: "plus.circle.fill"), tag: 2)
-        let tabBarItemDonations = UITabBarItem(title: "Donations", image: UIImage(systemName: "list.bullet.rectangle.portrait.fill"), tag: 3)
-        let tabBarItemMap = UITabBarItem(title: "Map", image: UIImage(systemName: "mappin.and.ellipse"), tag: 4)
+        let tabBarItemHome = UITabBarItem(title: "Panel", image: UIImage(systemName: "house.fill"), tag: 0)
+        let tabBarItemDonations = UITabBarItem(title: "Donacje", image: UIImage(systemName: "list.bullet.rectangle.portrait.fill"), tag: 1)
+        let tabBarItemAddDonation = UITabBarItem(title: "Dodaj wpis", image: UIImage(systemName: "plus.circle.fill"), tag: 2)
+        let tabBarItemMap = UITabBarItem(title: "Placówki", image: UIImage(systemName: "mappin.and.ellipse"), tag: 3)
+        let tabBarItemUser = UITabBarItem(title: "Profil", image: UIImage(systemName: "person.fill"), tag: 4)
         
         // Set the tab bar controller's tint color and translucency properties
         tabBarController.tabBar.tintColor = UIColor.systemBlue
@@ -53,10 +53,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Set the tab bar items of each navigation controller
         homeNC.tabBarItem = tabBarItemHome
-        userNC.tabBarItem = tabBarItemUser
-        addDonationNC.tabBarItem = tabBarItemAddDonation
         donationsNC.tabBarItem = tabBarItemDonations
+        addDonationNC.tabBarItem = tabBarItemAddDonation
         mapNC.tabBarItem = tabBarItemMap
+        userNC.tabBarItem = tabBarItemUser
         
         // Set the root view controller of the window to the tab bar controller
         window?.rootViewController = tabBarController
