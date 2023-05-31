328eb5432731093ed79a5be3afe255e127dd72b0
Łukasz Cettler <lukasz.cettler@gmail.com>
2023-05-31 17:56:19 +0200

    DEV-25 user login views (#6)
    * DEV-17 Add donation and disqualification view
    
    * DEV-83 WIP: Add broken NextDonationView Model, Views dir
    
    * DEV-83 NextDonationView and BloodTypeModal, add image assets (blood types & badges)
    
    * DEV-83 WIP: Set frame for NextDonationView, attempts at embedding that view into AppDelegate
    
    * Fix errors and add DataModel
    
    * Add Login and register views
    
    * Delete DonationDataModel data model
    
    ---------
    
    Co-authored-by: Adam Jurkiewicz <adam.jurkiewicz@senti1.com>
    Co-authored-by: Adam Jurkiewicz <a.jurkiewicz5@gmail.com>

---
+++ BloodDonor/Modules/Donation/DonationView.swift
@@ -0,0 +1,25 @@
+//
+//  DonationView.swift
+//  BloodDonor
+//
+//  Created by Łukasz Cettler on 30/05/2023.
+//
+
+import SwiftUI
+
+struct DonationView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.teal
+            }
+            .navigationTitle("Add Donation")
+        }
+    }
+}
+
+struct DonationView_Previews: PreviewProvider {
+    static var previews: some View {
+        DonationView()
+    }
+}
