a9408915cf7fb65673edd0c8f9438c789177e50f
Sylwester Kąkol <39565249+s-kakol@users.noreply.github.com>
2023-06-13 17:33:27 +0200

    Implement profileVM getUserProfile()

--- BloodDonor/ViewModel/ProfileViewModel.swift
+++ BloodDonor/ViewModel/ProfileViewModel.swift
@@ -11,8 +11,10 @@ import JWTDecode
 
 class ProfileViewModel: ObservableObject {
     
+    @Published var userInfo: UserInfo? = nil
     @Published var showAuthContainer = true
-    @Published var loginPending = false
+    @Published var loadingState: LoadingState = .notStarted
+    @Published var registerPending = false
     @Published var alert: IdentifiableAlert?
     
     func logout() {
@@ -23,7 +25,38 @@ class ProfileViewModel: ObservableObject {
         }
     }
     
-    func getUserProfile() {
-        
+    func getUserInfo () {
+        withAnimation {
+            loadingState = .loading
+        }
+        DispatchQueue.global(qos: .userInitiated).async {
+            Requester.shared.getUserInfo() { [self] result in
+                withAnimation {
+                    registerPending = false
+                }
+                
+                switch result {
+                case .success(let resultUserInfo):
+                    withAnimation {
+                        userInfo = resultUserInfo
+                        loadingState = .finished
+                    }
+                case .serverError(let err):
+                    withAnimation {
+                        loadingState = .error
+                    }
+                    alert = IdentifiableAlert.buildForError(id: "user_info__server_err", message: Errors.messageFor(err: err.message))
+                case .networkError(_):
+                    withAnimation {
+                        loadingState = .error
+                    }
+                    alert = IdentifiableAlert.networkError()
+                case .authError(_):
+                    withAnimation {
+                        self.showAuthContainer = true
+                    }
+                }
+            }
+        }
     }
 }
