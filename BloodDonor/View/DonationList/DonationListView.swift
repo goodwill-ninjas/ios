199b896928c159462a234ef176042dd15006e242
Lukasz Cettler <lukasz.cettler@gmail.com>
2023-06-15 18:24:18 +0200

    Fix DonationList Navigation Bar

--- BloodDonor/View/DonationList/DonationListView.swift
+++ BloodDonor/View/DonationList/DonationListView.swift
@@ -11,125 +11,128 @@ struct DonationListView: View {
     @EnvironmentObject var donationList: DonationListViewModel
     
     var body: some View {
-        VStack {
-            switch donationList.loadingState {
-            case .finished:
-                List() {
-                    ForEach(donationList.userDonations, id: \.id) { userDonation in
-                        VStack() {
-                            if (userDonation.disqualified) {
-                                Text("🚫 Dyskwalifikacja na \(userDonation.disqualification_days!) dni  ")
-                                    .font(.headline)
-                                    .frame(maxWidth: .infinity, alignment: .center)
-                                
-                                Text(trimDate(userDonation.donated_at))
-                                    .font(.caption2)
-                                    .italic()
-                                
-                                if (userDonation.details != nil) {
-                                    Text("\n\(userDonation.details!)")
-                                        .font(.caption)
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                }
-                            } else {
-                                Text("🩸 \(BloodTypeTranslator.translateBloodType(type: userDonation.donated_type)) - \(userDonation.amount)ml")
-                                    .font(.headline)
-                                    .frame(maxWidth: .infinity, alignment: .center)
-                                
-                                Text(trimDate(userDonation.donated_at))
-                                    .font(.caption2)
-                                    .italic()
-                                
-                                if(userDonation.hemoglobin != nil ||
-                                   userDonation.blood_pressure != nil) {
-                                    Spacer()
-                                }
-                                
-                                HStack() {
-                                    if (userDonation.hemoglobin != nil) {
-                                        VStack() {
-                                            Text("Poziom hemoglobiny")
-                                                .font(.caption)
-                                                .bold()
-                                                .frame(maxWidth: .infinity, alignment: .leading)
-                                            
-                                            Text("\(userDonation.hemoglobin!) g/l")
+        NavigationView {
+            VStack {
+                switch donationList.loadingState {
+                case .finished:
+                    List() {
+                        ForEach(donationList.userDonations, id: \.id) { userDonation in
+                            VStack() {
+                                if (userDonation.disqualified) {
+                                    Text("🚫 Dyskwalifikacja na \(userDonation.disqualification_days!) dni  ")
+                                        .font(.headline)
+                                        .frame(maxWidth: .infinity, alignment: .center)
+                                    
+                                    Text(trimDate(userDonation.donated_at))
+                                        .font(.caption2)
+                                        .italic()
+                                    
+                                    if (userDonation.details != nil) {
+                                        Text("\n\(userDonation.details!)")
+                                            .font(.caption)
+                                            .frame(maxWidth: .infinity, alignment: .leading)
+                                    }
+                                } else {
+                                    Text("🩸 \(BloodTypeTranslator.translateBloodType(type: userDonation.donated_type)) - \(userDonation.amount)ml")
+                                        .font(.headline)
+                                        .frame(maxWidth: .infinity, alignment: .center)
+                                    
+                                    Text(trimDate(userDonation.donated_at))
+                                        .font(.caption2)
+                                        .italic()
+                                    
+                                    if(userDonation.hemoglobin != nil ||
+                                       userDonation.blood_pressure != nil) {
+                                        Spacer()
+                                    }
+                                    
+                                    HStack() {
+                                        if (userDonation.hemoglobin != nil) {
+                                            VStack() {
+                                                Text("Poziom hemoglobiny")
+                                                    .font(.caption)
+                                                    .bold()
+                                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                                
+                                                Text("\(userDonation.hemoglobin!) g/l")
+                                                    .font(.caption)
+                                                    .italic()
+                                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                            }
+                                        }
+                                        
+                                        if (userDonation.blood_pressure != nil) {
+                                            VStack() {
+                                                Text("Ciśnienie krwi")
+                                                    .font(.caption)
+                                                    .bold()
+                                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                                                
+                                                Text("\(userDonation.blood_pressure!) mm Hg")
+                                                    .font(.caption)
+                                                    .italic()
+                                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                                            }
+                                        }
+                                    }
+                                    
+                                    if let arm = userDonation.arm {
+                                        Spacer()
+                                        if let conjugatedArm = ArmConjugation(rawValue: arm) {
+                                            Text("Ręka użyta do pobrania: \(conjugatedArm.conjugateArm())")
                                                 .font(.caption)
                                                 .italic()
                                                 .frame(maxWidth: .infinity, alignment: .leading)
                                         }
                                     }
                                     
-                                    if (userDonation.blood_pressure != nil) {
-                                        VStack() {
-                                            Text("Ciśnienie krwi")
-                                                .font(.caption)
-                                                .bold()
-                                                .frame(maxWidth: .infinity, alignment: .trailing)
-                                            
-                                            Text("\(userDonation.blood_pressure!) mm Hg")
-                                                .font(.caption)
-                                                .italic()
-                                                .frame(maxWidth: .infinity, alignment: .trailing)
-                                        }
-                                    }
-                                }
-                                
-                                if let arm = userDonation.arm {
-                                    Spacer()
-                                    if let conjugatedArm = ArmConjugation(rawValue: arm) {
-                                        Text("Ręka użyta do pobrania: \(conjugatedArm.conjugateArm())")
+                                    if (userDonation.details != nil) {
+                                        Text("\n\(userDonation.details!)")
                                             .font(.caption)
-                                            .italic()
                                             .frame(maxWidth: .infinity, alignment: .leading)
                                     }
                                 }
-                                
-                                if (userDonation.details != nil) {
-                                    Text("\n\(userDonation.details!)")
-                                        .font(.caption)
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                }
                             }
                         }
+                        .onDelete(perform: deleteItem)
+                        
                     }
-                    .onDelete(perform: deleteItem)
-                    
-                }
-                .onAppear() {
-                    donationList.getUserDonations()
-                }
-            case .error:
-                Spacer()
-                Text("An error occurred while loading data")
+                    .onAppear() {
+                        donationList.getUserDonations()
+                    }
+                case .error:
+                    Spacer()
+                    Text("An error occurred while loading data")
+                        .padding(.horizontal, 24)
+                    Button {
+                        donationList.getUserDonations()
+                    } label: {
+                        Text("Retry")
+                            .foregroundColor(Color.white)
+                            .fontWeight(.bold)
+                            .padding(.vertical, 12)
+                            .padding(.horizontal, 50)
+                            .background(Color.black)
+                    }
+                    .cornerRadius(10)
+                    .padding(.top, 16)
                     .padding(.horizontal, 24)
-                Button {
-                    donationList.getUserDonations()
-                } label: {
-                    Text("Retry")
-                        .foregroundColor(Color.white)
-                        .fontWeight(.bold)
-                        .padding(.vertical, 12)
-                        .padding(.horizontal, 50)
-                        .background(Color.black)
+                    Spacer()
+                case .notStarted, .loading:
+                    Spacer()
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle(tint: .black))
+                        .scaleEffect(1.6)
+                        .padding(.top, 26)
+                    Spacer()
                 }
-                .cornerRadius(10)
-                .padding(.top, 16)
-                .padding(.horizontal, 24)
-                Spacer()
-            case .notStarted, .loading:
-                Spacer()
-                ProgressView()
-                    .progressViewStyle(CircularProgressViewStyle(tint: .black))
-                    .scaleEffect(1.6)
-                    .padding(.top, 26)
-                Spacer()
             }
-        }.navigationBarTitle("Dziennik Donacji", displayMode: .inline)
+            .navigationBarTitle("Dziennik Donacji")
             .onAppear {
-                if donationList.userDonations.isEmpty {
-                    donationList.getUserDonations()
-            }
+                    if donationList.userDonations.isEmpty {
+                        donationList.getUserDonations()
+                    }
+                }
         }
     }
     let trimDate: (String) -> String = { date in
@@ -141,3 +144,4 @@ struct DonationListView: View {
         donationList.userDonations.remove(atOffsets: offsets)
     }
 }
+
